cc719245df136abe7720d317e3428887be0f55b0
Nicholas Hegarty <nichhega@stjohns.school.nz>
2026-03-16 12:13:00 +1300

    second commit

--- Insurance_App_2026/Program.cs
+++ Insurance_App_2026/Program.cs
@@ -2,6 +2,21 @@
 {
     public class Program
     {
+        // Global Constants
+        public const decimal DISCOUNT = 0.9m;
+        public const decimal DEPRECIATION = 0.95m;
+
+        // Global Variables
+        public static decimal totalInsurance = 0.0m;
+        public static string topDevice = "";
+        public static decimal topCost = 0.0m;
+
+        public static int deviceCounter = 0;
+        public static string reports = "";
+
+        public static List<string> deviceCategories =
+            new List<string> { "Laptop", "Desktop", "Other" };
+
         static void Main(string[] args)
         {
             Console.WriteLine(" /$$$$$$                                                                                       /$$$$$$                     \r\n|_  $$_/                                                                                      /$$__  $$                    \r\n  | $$   /$$$$$$$   /$$$$$$$ /$$   /$$  /$$$$$$  /$$$$$$  /$$$$$$$   /$$$$$$$  /$$$$$$       | $$  \\ $$  /$$$$$$   /$$$$$$ \r\n  | $$  | $$__  $$ /$$_____/| $$  | $$ /$$__  $$|____  $$| $$__  $$ /$$_____/ /$$__  $$      | $$$$$$$$ /$$__  $$ /$$__  $$\r\n  | $$  | $$  \\ $$|  $$$$$$ | $$  | $$| $$  \\__/ /$$$$$$$| $$  \\ $$| $$      | $$$$$$$$      | $$__  $$| $$  \\ $$| $$  \\ $$\r\n  | $$  | $$  | $$ \\____  $$| $$  | $$| $$      /$$__  $$| $$  | $$| $$      | $$_____/      | $$  | $$| $$  | $$| $$  | $$\r\n /$$$$$$| $$  | $$ /$$$$$$$/|  $$$$$$/| $$     |  $$$$$$$| $$  | $$|  $$$$$$$|  $$$$$$$      | $$  | $$| $$$$$$$/| $$$$$$$/\r\n|______/|__/  |__/|_______/  \\______/ |__/      \\_______/|__/  |__/ \\_______/ \\_______/      |__/  |__/| $$____/ | $$____/ \r\n                                                                                                       | $$      | $$      \r\n                                                                                                       | $$      | $$      \r\n                                                                                                       |__/      |__/      ");
