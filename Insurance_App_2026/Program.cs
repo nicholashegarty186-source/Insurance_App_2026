0f2d7089d22408f23a84facd2c935fa46ee37057
Nicholas Hegarty <nichhega@stjohns.school.nz>
2026-03-20 10:24:20 +1300

    commit 1.2

--- Insurance_App_2026/Program.cs
+++ Insurance_App_2026/Program.cs
@@ -10,17 +10,17 @@
         public static decimal totalInsurance = 0.0m;
         public static string topDevice = "";
         public static decimal topCost = 0.0m;
+        public static string CheckName = "";
 
         public static int deviceCounter = 0;
         public static string reports = "";
 
         public static List<string> deviceCategories =
-        new List<string> { "Laptop", "Desktop", "Other" };
+            new List<string> { "Laptop", "Desktop", "Other" };
 
         static void Main(string[] args)
-       static void Main(string[] args)
         {
-            ShowTitleScreen(Console.WriteLine(" /$$$$$$                                                                                       /$$$$$$                     \r\n|_  $$_/                                                                                      /$$__  $$                    \r\n  | $$   /$$$$$$$   /$$$$$$$ /$$   /$$  /$$$$$$  /$$$$$$  /$$$$$$$   /$$$$$$$  /$$$$$$       | $$  \\ $$  /$$$$$$   /$$$$$$ \r\n  | $$  | $$__  $$ /$$_____/| $$  | $$ /$$__  $$|____  $$| $$__  $$ /$$_____/ /$$__  $$      | $$$$$$$$ /$$__  $$ /$$__  $$\r\n  | $$  | $$  \\ $$|  $$$$$$ | $$  | $$| $$  \\__/ /$$$$$$$| $$  \\ $$| $$      | $$$$$$$$      | $$__  $$| $$  \\ $$| $$  \\ $$\r\n  | $$  | $$  | $$ \\____  $$| $$  | $$| $$      /$$__  $$| $$  | $$| $$      | $$_____/      | $$  | $$| $$  | $$| $$  | $$\r\n /$$$$$$| $$  | $$ /$$$$$$$/|  $$$$$$/| $$     |  $$$$$$$| $$  | $$|  $$$$$$$|  $$$$$$$      | $$  | $$| $$$$$$$/| $$$$$$$/\r\n|______/|__/  |__/|_______/  \\______/ |__/      \\_______/|__/  |__/ \\_______/ \\_______/      |__/  |__/| $$____/ | $$____/ \r\n                                                                                                       | $$      | $$      \r\n                                                                                                       | $$      | $$      \r\n                                                                                                       |__/      |__/      ");
+            ShowTitleScreen();
 
             char continueInput = 'y';
 
@@ -42,4 +42,15 @@
             ShowSummary();
         }
 
-      
+        //show title screen
+        static void ShowTitleScreen()
+        {
+
+
+
+        }
+
+    
+    
+    }
+}
---
+++ Insurance_App_2026/ProgramHelpers.cs
@@ -0,0 +1,40 @@
+﻿namespace Wages_App
+{
+    internal static class ProgramHelpers
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" /$$$$$$                                                                                       /$$$$$$                     \r\n|_  $$_/                                                                                      /$$__  $$                    \r\n  | $$   /$$$$$$$   /$$$$$$$ /$$   /$$  /$$$$$$  /$$$$$$  /$$$$$$$   /$$$$$$$  /$$$$$$       | $$  \\ $$  /$$$$$$   /$$$$$$ \r\n  | $$  | $$__  $$ /$$_____/| $$  | $$ /$$__  $$|____  $$| $$__  $$ /$$_____/ /$$__  $$      | $$$$$$$$ /$$__  $$ /$$__  $$\r\n  | $$  | $$  \\ $$|  $$$$$$ | $$  | $$| $$  \\__/ /$$$$$$$| $$  \\ $$| $$      | $$$$$$$$      | $$__  $$| $$  \\ $$| $$  \\ $$\r\n  | $$  | $$  | $$ \\____  $$| $$  | $$| $$      /$$__  $$| $$  | $$| $$      | $$_____/      | $$  | $$| $$  | $$| $$  | $$\r\n /$$$$$$| $$  | $$ /$$$$$$$/|  $$$$$$/| $$     |  $$$$$$$| $$  | $$|  $$$$$$$|  $$$$$$$      | $$  | $$| $$$$$$$/| $$$$$$$/\r\n|______/|__/  |__/|_______/  \\______/ |__/      \\_______/|__/  |__/ \\_______/ \\_______/      |__/  |__/| $$____/ | $$____/ \r\n                                                                                                       | $$      | $$      \r\n                                                                                                       | $$      | $$      \r\n                                                                                                       |__/      |__/      ");
+            Console.WriteLine("How many Devices do you want to enter+");
+        }
+
+
+        static void Main(string[] args)
+        {
+            {
+
+                char continueInput = 'y';
+
+                while (continueInput == 'y')
+                {
+                    Console.Clear();
+
+                    Console.WriteLine(ProcessDevice());
+
+                    continueInput = CheckContinueInput(
+                        "\nDo you want to enter another device? (y/n)"
+                    );
+                }
+
+                Console.Clear();
+
+                Console.WriteLine(Program.reports);
+                Program.
+                            ShowSummary();
+
+
+
+            }
+    }
+}
